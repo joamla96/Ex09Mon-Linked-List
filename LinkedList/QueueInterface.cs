65c76a7ce61f35986b16eefe4b7589f45578300c
jona8690 <jona8690@edu.eal.dk>
2017-02-28 14:25:48 +0100

    Changed interface to deal with icomparable as well, since our implementation of mylist uses that for the sort.

--- LinkedList/QueueInterface.cs
+++ LinkedList/QueueInterface.cs
@@ -9,18 +9,17 @@ namespace LinkedList {
 		bool isEmpty();
 		//Pre: none
 		//Post: Returns true if the queue is empty, otherwise returns false.
-		void enqueue(Object item);
+		void enqueue(IComparable item);
 		//Pre:item is the new item to be added
 		//Post: If insertion is successful, item is at the end of the queue.
 		//Post:Throw QueueException if the item cannot be added to the queue.
-		Object front();
+		IComparable front();
 		//Pre: none
 		//Post: If queue is not empty, the item at the front of a queue is returned, and the queue
 		//Post: is left unchanged. Throws QueueException if the queue is empty.
-		Object dequeue();
+		IComparable dequeue();
 		//Pre: none
 		//Post: If queue is not empty, the item at the front of the queue is retrieved and removed
 		//Post: from the queue. Throws QueueException if the queue is empty.
 	}
-
 }
